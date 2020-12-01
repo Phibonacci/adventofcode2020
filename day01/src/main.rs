e1b4fc8d4555bc35030d95ee9d68d91cf1be8742
Jean Fauquenot <jean.fauquenot@gmail.com>
2020-12-01 20:25:09 +0100

    day1: reformat file with rust tools

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,42 +7,45 @@ fn main() {
   let filename = &args[1];
   println!("Loading file {}", filename);
   let data = parse_file(filename);
-  part1(& data);
-  part2(& data);
+  part1(&data);
+  part2(&data);
   println!("Total elapsed time: {:.2?}", before.elapsed());
 }
 
-fn parse_file(filename : impl AsRef<std::path::Path>) -> Vec<u32> {
+fn parse_file(filename: impl AsRef<std::path::Path>) -> Vec<u32> {
   let before = std::time::Instant::now();
   let file = std::fs::File::open(filename).expect("File not found");
   let buf = std::io::BufReader::new(file);
   use std::io::prelude::*;
-  let content = buf.lines()
-      .map(|l| l.expect("Could not parse line"))
-      .map(|l| l.parse::<u32>().expect("Could not parse number"))
-      .collect();
+  let content = buf
+    .lines()
+    .map(|l| l.expect("Could not parse line"))
+    .map(|l| l.parse::<u32>().expect("Could not parse number"))
+    .collect();
   println!("Parsing: elapsed time: {:.2?}", before.elapsed());
   content
 }
 
-fn part1(data: & Vec<u32>) -> () {
+fn part1(data: &Vec<u32>) -> () {
   let before = std::time::Instant::now();
   for index_left_hand in 0..(data.len() - 1) {
     for index_right_hand in index_left_hand..data.len() {
       let left_hand = data[index_left_hand];
       let right_hand = data[index_right_hand];
       if left_hand + right_hand == 2020 {
-        println!("Result part1: {:>10} | elapsed time: {:.2?}",
+        println!(
+          "Result part1: {:>10} | elapsed time: {:.2?}",
           left_hand * right_hand,
-          before.elapsed());
-        return
+          before.elapsed()
+        );
+        return;
       }
     }
   }
-  println!("Part2: result not found");
+  println!("Part1: result not found");
 }
 
-fn part2(data: & Vec<u32>) -> () {
+fn part2(data: &Vec<u32>) -> () {
   let before = std::time::Instant::now();
   for index_first_number in 0..(data.len() - 2) {
     for index_second_number in index_first_number..data.len() {
@@ -51,10 +54,12 @@ fn part2(data: & Vec<u32>) -> () {
         let second_number = data[index_second_number];
         let third_number = data[index_third_number];
         if first_number + second_number + third_number == 2020 {
-          println!("Result part2: {:>10} | elapsed time: {:.2?}",
+          println!(
+            "Result part2: {:>10} | elapsed time: {:.2?}",
             first_number * second_number * third_number,
-            before.elapsed());
-          return
+            before.elapsed()
+          );
+          return;
         }
       }
     }
