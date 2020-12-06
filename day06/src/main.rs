b534bc773e39614495e66725b5ff7426f0c0204f
Jean Fauquenot <jean.fauquenot@gmail.com>
2020-12-06 18:28:03 +0100

    day06: factorise both parts but slow them down

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,39 +4,38 @@ fn main() {
   if args.len() < 2 {
     panic!("Not enough arguments");
   }
-  let filename = &args[1];
-  let declarations: Vec<String> = std::fs::read_to_string(filename)
-    .expect(&format!("File \"{}\" not found", filename))
+  let declarations: Vec<Vec<String>> = std::fs::read_to_string(&args[1])
+    .expect(&format!("File \"{}\" not found", &args[1]))
     .split("\n\n")
     .map(|l| l.to_string())
+    .map(|s| s.lines().map(|l| l.to_string()).collect())
     .collect();
 
-  let p1 = declarations
-    .iter()
-    .map(|s| s.chars().into_iter().filter(|c| *c != '\n').collect())
-    .fold(0, |total, group: std::collections::HashSet<char>| {
-      total + group.len()
-    });
+  let p1 = count(&declarations, false);
   println!("Part1: {} |  elapsed time: {:.2?}", p1, before_p1.elapsed());
   let before_p2 = std::time::Instant::now();
-  let p2 = declarations
+  let p2 = count(&declarations, true);
+  println!("Part2: {} |  elapsed time: {:.2?}", p2, before_p2.elapsed());
+}
+
+fn count(declarations: &Vec<Vec<String>>, unanimous: bool) -> usize {
+  declarations
     .iter()
-    .map(|s| s.lines().map(|l| l.to_string()).collect())
-    .fold(0, |sum, group: Vec<String>| {
+    .map(|group| {
       group
         .iter()
         .fold(
           group[0].chars().collect(),
-          |unanimous: Vec<char>, current| {
+          |acc: std::collections::HashSet<char>, current| {
             current
               .chars()
               .into_iter()
-              .filter(|c| unanimous.contains(c))
+              .chain(acc.clone())
+              .filter(|c| !unanimous || (acc.contains(c) && current.contains(*c)))
               .collect()
           },
         )
         .len()
-        + sum
-    });
-  println!("Part2: {} |  elapsed time: {:.2?}", p2, before_p2.elapsed());
+    })
+    .sum()
 }
