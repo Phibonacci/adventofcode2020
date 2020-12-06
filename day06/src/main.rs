1a9ee68469f9d6db88bc8426cba6c584e7abae49
Jean Fauquenot <jean.fauquenot@gmail.com>
2020-12-06 19:42:47 +0100

    day06: simplify the code

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,42 +1,33 @@
+use std::collections::HashSet;
+
 fn main() {
   let before_p1 = std::time::Instant::now();
   let args: Vec<String> = std::env::args().collect();
   if args.len() < 2 {
     panic!("Not enough arguments");
   }
-  let declarations: Vec<Vec<String>> = std::fs::read_to_string(&args[1])
-    .expect(&format!("File \"{}\" not found", &args[1]))
-    .split("\n\n")
-    .map(|l| l.to_string())
-    .map(|s| s.lines().map(|l| l.to_string()).collect())
-    .collect();
+  let buffer = std::fs::read_to_string(&args[1]).unwrap();
+  let declarations: Vec<&str> = buffer.split("\n\n").collect();
 
-  let p1 = count(&declarations, |a, b| a | b);
+  let p1: usize = declarations
+    .iter()
+    .map(|group| count(group, |a, b| a | b))
+    .sum();
   println!("Part1: {} |  elapsed time: {:.2?}", p1, before_p1.elapsed());
   let before_p2 = std::time::Instant::now();
-  let p2 = count(&declarations, |a, b| a & b);
+  let p2: usize = declarations
+    .iter()
+    .map(|group| count(group, |a, b| a & b))
+    .sum();
   println!("Part2: {} |  elapsed time: {:.2?}", p2, before_p2.elapsed());
 }
 
-fn count(
-  declarations: &Vec<Vec<String>>,
-  op: fn(
-    &std::collections::HashSet<char>,
-    &std::collections::HashSet<char>,
-  ) -> std::collections::HashSet<char>,
-) -> usize {
-  declarations
+fn count(group: &str, op: fn(&HashSet<char>, &HashSet<char>) -> HashSet<char>) -> usize {
+  let lines: Vec<&str> = group.lines().collect();
+  lines
     .iter()
-    .map(|group| {
-      group
-        .iter()
-        .fold(
-          group[0].chars().collect(),
-          |acc: std::collections::HashSet<char>, current| {
-            op(&current.chars().into_iter().collect(), &acc)
-          },
-        )
-        .len()
+    .fold(lines[0].chars().collect(), |acc: HashSet<char>, current| {
+      op(&current.chars().into_iter().collect(), &acc)
     })
-    .sum()
+    .len()
 }
